1425398074900f86aad2e053bcdb526cf6936f78
Rosty Semenyshyn <ripka.ua@gmail.com>
2021-04-02 22:42:11 +0200

    working example

---
+++ amplify-ssr/amplify/backend/function/amplifyssr/src/app.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+});
+
+app.get('*', function(req, res) {
+    app.use(express.static(path.join(__dirname, './client/build')));
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
+
+module.exports = app
